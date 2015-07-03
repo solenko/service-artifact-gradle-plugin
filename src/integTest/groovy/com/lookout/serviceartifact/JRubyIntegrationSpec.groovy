9611606b31fe65123279666c81eb61f4bac48782
R. Tyler Croy <tyler@monkeypox.org>
2015-07-03 08:47:25 -0700

    Start using the nebula-test plugin for integration testing
    Also adding an integration test which actually verifies the production of a tar
    file
    
    See <https://github.com/nebula-plugins/nebula-test>

--- src/integTest/groovy/com/lookout/serviceartifact/JRubyIntegrationSpec.groovy
+++ src/integTest/groovy/com/lookout/serviceartifact/JRubyIntegrationSpec.groovy
@@ -6,21 +6,20 @@ import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
 import org.gradle.testfixtures.ProjectBuilder
 
+import nebula.test.PluginProjectSpec
+import nebula.test.IntegrationSpec
+
 import com.github.jrubygradle.jar.JRubyJar
 
 /**
  * Verify that the end-to-end JRuby support works
  */
-class JRubyIntegrationSpec extends Specification {
-    protected Project project
+class JRubyIntegrationSpec extends PluginProjectSpec {
     protected String serviceName = 'faas'
     protected String componentName = 'backend'
-    protected String version = '1.1'
+    protected String version     = '1.1'
 
-    void setup() {
-        project = ProjectBuilder.builder().build()
-        project.apply plugin: 'com.github.lookout.service-artifact'
-    }
+    String getPluginName() { 'com.github.lookout.service-artifact' }
 
     boolean hasJRubyPlugins(Project project) {
         return (project.plugins.findPlugin('com.github.jruby-gradle.base') &&
@@ -28,6 +27,7 @@ class JRubyIntegrationSpec extends Specification {
     }
 
     void enableJRuby() {
+        project.apply plugin: this.pluginName
         project.version = version
         project.service {
             name this.serviceName
@@ -58,5 +58,32 @@ class JRubyIntegrationSpec extends Specification {
         then:
         config
         config.artifacts.find { it.archiveTask instanceof JRubyJar }
+    }
+}
+
+class JRubyFullIntegrationSpec extends IntegrationSpec {
+
+    def "the assemble task should produce a tar"() {
+        given:
+        String version = '1.0'
+        String projectName = 'fullinteg'
+        settingsFile << "rootProject.name = '${projectName}'"
+        buildFile << """
+apply plugin: 'com.github.lookout.service-artifact'
+
+version = '${version}'
+
+service {
+  name '${projectName}'
+  component('api', type: JRuby) { }
+}
+"""
+
+        /* XXX: why do I have to make the buildDir myself? */
+        directory('build')
+
+        expect:
+        runTasksSuccessfully('assemble')
+        fileExists("build/distributions/${projectName}-${version}.tar")
     }
 }
